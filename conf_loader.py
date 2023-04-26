d90f7945a34e92b2c51ba514a6db88115a9d0b80
bison-- <mike.bison42@googlemail.com>
2023-04-27 01:43:50 +0200

    typo, wrong exception

--- conf_loader.py
+++ conf_loader.py
@@ -2,7 +2,7 @@ from conf import *
 
 try:
     from conf_local import *
-except FileNotFoundError:
-    # local conf does not exist is in theory valid, but the ACCESS_TOKEN shoulöd not be set in conf.py
+except ModuleNotFoundError:
+    # local_conf.py does not exist is in theory valid, but the ACCESS_TOKEN should not be set in conf.py
     print('WARNING: conf_local.py does not exist.')
 
