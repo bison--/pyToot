36b711d74fe5c221e7054a917546dc4a30579688
bison-- <mike.bison42@googlemail.com>
2023-04-28 00:54:54 +0200

    notes

--- conf.py
+++ conf.py
@@ -1,3 +1,7 @@
+# DON'T make changes here
+# copy this file to `conf_local.py` and make your changes there
+# recommended: only have variables in `conf_local.py` which you actually change
+
 ACCESS_TOKEN = '<your access token HERE>'
 INSTANCE_DOMAIN = 'mastodon.social'
 LANGUAGE = 'en'
